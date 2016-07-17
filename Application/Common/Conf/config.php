5b4cf068bf6deeae8ae362e29d0e6c1138c94b22
jay_tian <806338233@qq.com>
2016-07-17 17:32:24 +0800

    fix

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,6 @@
 <?php
 return array(
+	'ACTION_SUFFIX'         =>  'Action',
+	'DEFAULT_V_LAYER'       =>  'View',
 	//'配置项'=>'配置值'
 );
---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,79 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html> 
+<html class=""> 
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1">
+  <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+  <meta name="renderer" content="webkit">
+  <title>  首页 -记录生活,感受美好-Powered By tsj  </title>
+  <meta name="keywords" content="记录生活 IT知识 ThinkPhp mysql php bootstrap react" />
+   <meta name="description" content="每天记录一点，成长一点，全栈工程师之路" /> 
+
+
+	  <link href="/Public/css/bootstrap.css" rel="stylesheet" />
+	  <link href="/Public/css/index.css" rel="stylesheet" />   
+
+
+	  <!--[if lt IE 9]>
+	    <script src="/Public/js/html5shiv.js"></script>
+	  <![endif]-->
+	  <!--[if IE 8]>
+	    <script src="/Public/js/respond.js"></script>
+	  <![endif]--> 
+	<script src="/Public/js/jquery.js"></script>
+	<script src="/Public/js/bootstrap.js"></script>
+
+</head>
+	<body>
+		
+<div class="index">
+	<div class="banner">
+		<h1>永远不停止向前奔跑！</h1>
+	</div>
+	<nav class="main-navigation">
+		<ul class="menu container">
+		    <li class="act" role="presentation"><a href="/">首页</a></li>
+		  <!--   <li role="presentation"><a href="" title="" target="_blank"></a></li> -->
+		</ul>   
+    </nav>
+    <div class="content container">
+    	<div class="row">
+    		<div class="col-sm-9">
+				<article class="artcile">
+			        <h1><a href="/post/laravel-turns-five/">Laravel 五岁了</a></h1>
+			        <div class="autor">
+			            <span class="author">作者：<a href="/author/wangsai/">王赛</a></span> •
+			            <time class="post-date" datetime="2016年6月12日星期日凌晨2点49分" title="2016年6月12日星期日凌晨2点49分">2016年6月12日</time>
+			        </div>
+				    <div class="img">
+				        <a href="/post/laravel-turns-five/">
+				        <img src="http://image.golaravel.com/e/09/f05fc8d95497bad948ac46eb68d9d.jpg" alt="Laravel 五岁了"></a>
+				    </div>
+				    <div class="sub">
+				        <p>五年前（2011年6月11日）的今天，Taylor Otwell 将 Laravel 带到了这个世界！ 下面摘抄了部分原文：点这里看完整的原文   I've been working on this framework for about 7 months. I've worked really hard to mak</p>
+				    </div>
+				    <a href="/post/laravel-turns-five/" class="btn btn-default">阅读全文</a>
+				</article>
+    		</div>
+    		<div class="col-sm-3">
+				<div class="attend text-center">
+				       <div class="box clearfix" id="scrollspy">
+				          <div class="author">
+				             <a> <img class="avatar-md" src="/Public/img/autor.jpg" alt="weixinvcbzv"></a>
+				          </div>
+				          <div class="person-info">
+				          	<p>QQ: 806338233</p>
+				          	<p>公司: EDUSOHO</p>
+				          	<p>芳龄： 25</p>
+				          	<p>目标： 成为一个全栈工程师</p>
+				          </div>
+				       </div>
+				  </div>
+
+    		</div>
+    	</div>
+    </div>
+</div>
+
+	</body>
+</html>
